5fc057132c5641635f86a8bab7a111375dd398b1
s_iceman <s_iceman@protonmail.com>
2022-01-26 00:08:34 +0300

    feat: implement brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = bracketsConfig.reduce((acc, [k, v]) => ({...acc, [k]: v}), {});
+  let stack = [];
+
+  [...str].forEach(bracket => {
+    stack.push(bracket);
+    if (stack.length > 1){
+      if (config[stack[stack.length - 2]] === stack[stack.length - 1]){
+        stack.splice(stack.length - 2, 2);
+      }
+    }
+  });
+
+  return stack.length == 0;
 }
